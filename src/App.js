28a71df33fea3be586a623c5b424367287a580ac
Михаил Литийский < >
2017-10-24 18:53:05 +0300

    'load start pause'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 
@@ -7,25 +7,32 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      player: new window.Audio(),
       track: '',
+      isPause: false,
     }
   }
 
   componentDidMount() {
-
   }
 
-  playSong() {
-    let player = new window.Audio();
-
+  loadTrack() {
     fetch('/api/track')
     .then(function(response) {
       return response.blob();
     }).then(function(blob){
-      player.src = window.URL.createObjectURL(blob);
+      this.state.player.src = window.URL.createObjectURL(blob);
       //player.playbackRate = 0.8;
-      player.play();
-    })
+      //this.state.player.play();
+    }.bind(this))
+  }
+
+  startTrack() {
+    this.state.player.play();
+  }
+
+  pauseTrack() {
+    this.state.player.pause();
   }
 
   render() {
@@ -35,8 +42,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <span onClick={()=>{this.playSong()}}>test</span>
-
+        <span onClick={()=>{this.loadTrack()}}> Load </span>
+        <span onClick={()=>{this.startTrack()}}> Start </span>
+        <span onClick={()=>{this.pauseTrack()}}> Pause </span>
       </div>
     );
   }
