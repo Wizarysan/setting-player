27249b317434e28b3d8be0ec7f08a631fe0f6d0c
Wizary <wizaryn@gmail.com>
2018-03-26 01:09:11 +0300

    'Beta 1'

--- public/server.js
+++ public/server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const parseUrl = require('parseurl');
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
+const NodeID3 = require('node-id3');
+const mp3Duration = require('mp3-duration');
 
 var app = express();
 
@@ -11,8 +14,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '')));
 
-app.all('/api/track', (req, res) => {  
+app.all('/api/track', (req, res) => {
   res.sendFile(path.join(__dirname, 'music/', req.body.track));
+})
+
+app.all('/api/playlist', (req, res) => {
+
+  let results = [],
+      promises = [];
+  // {
+  //       artist: tags.artist,
+  //       title: tags.title,
+  //       length,
+  //     }
+      //lenghtReadyFlag = true;
+
+  fs.readdir(path.join(__dirname, 'music/'), (err, files)=> {
+    files.map(function(filename){
+      let file = path.join(__dirname, 'music/', filename),
+          tags = NodeID3.read(file),
+          length = 0;
+
+          //Refactor
+          let lgPromise = mp3Duration(file, function (err, duration) {
+            if (err) return console.log(err.message);
+            //lenghtReadyFlag = false;
+          });
+
+          promises.push(lgPromise);
+
+          lgPromise.then(length=>{
+            results.push({
+              artist: tags.artist,
+              name: tags.title,
+              file: filename,
+              length,
+            })
+            // console.log(results)
+          })
+    })
+
+    console.log(promises)
+    Promise.all(promises).then(()=> {
+      console.log('all resolved')
+      res.send(results);
+    })
+  })
+
+
+
+
+
+  // let result = NodeID3.read(file, function(err, tags) {return tags;})
+  // console.log(result)
+
 })
 
 app.get('*', (req, res) => {
--- src/features/Playlist/index.js
+++ src/features/Playlist/index.js
@@ -33,6 +33,33 @@ class Playlist extends Component {
     }
   }
 
+  componentDidMount() {
+    //reach to API and create playlist from folder
+    //Set first track to play
+    fetch('/api/playlist', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(
+      //   {track: trackName}
+      // )
+    }).then(response => {
+
+      response.json().then(list=>{
+        console.log(list)
+        this.setState({
+          playingIndex: 0,
+          statePlaylist: {
+            name: 'test playlist',
+            tracks: list,
+          }
+        })
+      })
+    })
+  }
+
   componentWillReceiveProps(nextProps) {
     if(nextProps.nextTrack) {
       //TODO last track in playlist
